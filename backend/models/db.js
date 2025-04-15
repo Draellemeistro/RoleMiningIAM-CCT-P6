a0082ea0c0bdf3cd004859c483a9560a2578504d
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2025-04-15 15:06:40 +0200

    OBS ændret DB.. burde virke præcist ens for services osv, men hvis det bricker så er det her nok årsagen

--- backend/models/db.js
+++ backend/models/db.js
@@ -4,16 +4,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = await mysql.createConnection({
-  host: process.env.DB_URL,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  ssl: {
-    ca: fs.readFileSync('./ssl/DigiCertGlobalRootCA.crt.pem')
-  }
-});
+const createDB = async () => {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+      ca: fs.readFileSync('./ssl/DigiCertGlobalRootCA.crt.pem')
+    }
+  });
 
-console.log('✅ Connected to DB with async/await (promise-based)');
-export default connection;
+  console.log('✅ Connected to DB with async/await (promise-based)');
+
+  return {
+    query: (...args) => connection.query(...args),
+    end: () => connection.end()
+  };
+};
+
+export default await createDB();
